9edc29a2932a112a2ba8a7687109c132f4302d09
Aitor <aitor.martin06@somo.eus>
2026-02-22 12:57:15 +0100

    Añadido código completo de todas las clases - métodos, atributos y lógica

--- src/RETO2/Libro.java
+++ src/RETO2/Libro.java
@@ -2,4 +2,86 @@ package RETO2;
 
 public class Libro {
 
+    // Atributos
+    private int idLibro;
+    private String titulo;
+    private String isbn;
+    private int anioPublicacion;
+    private String genero;
+    private Autor autor;
+
+    // Constructor vacío
+    public Libro() {
+    }
+
+    // Constructor completo
+    public Libro(int idLibro, String titulo, String isbn, int anioPublicacion, String genero, Autor autor) {
+        this.idLibro = idLibro;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.anioPublicacion = anioPublicacion;
+        this.genero = genero;
+        this.autor = autor;
+    }
+
+    // Getters y Setters
+    public int getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "idLibro=" + idLibro +
+                ", titulo='" + titulo + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                ", genero='" + genero + '\'' +
+                ", autor=" + autor.getNombre() +
+                '}';
+    }
 }
--- src/RETO2/Main.java
+++ src/RETO2/Main.java
@@ -2,4 +2,8 @@ package RETO2;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Biblioteca b = new Biblioteca();
+        b.menu();
+    }
 }
